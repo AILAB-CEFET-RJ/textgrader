f6ecd1adf7c79f62b356c30f57662041f7e05725
cassiofb-dev <cassiofb.souza@gmail.com>
2023-06-17 23:12:37 -0300

    feat: use larger database

--- backend/src/requirements.py
+++ backend/src/requirements.py
@@ -5,7 +5,7 @@ import pandas as pd
 from urllib import request
 
 def setup_ntlk():
-  nltk.download('punkt')
+  nltk.download('punkt', download_dir='/backend/src/datalake/nltk_data')
 
   print('NLTK PUNKT setup finished')
 
@@ -39,20 +39,9 @@ def create_default_datalake():
 def download_and_convert_uol_corpus_essays():
   if os.path.isfile(ESSAY_FILE) == True: return
 
-  CORPUS_ESSAYS_JSON_LINKS = [
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_01.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_02.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_03.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_04.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_05.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_06.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_07.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_08.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_09.json',
-    'https://raw.githubusercontent.com/cassiofb-dev/corpus-redacoes-uol/master/corpus/uoleducacao_redacoes_10.json',
-  ]
+  CORPUS_ESSAYS_JSON_LINK = 'https://raw.githubusercontent.com/cassiofb-dev/web-scraper-uol-banco-de-redacoes/master/uol_redacoes.json'
 
-  CORPUS_ESSAYS_JSONS = [requests.get(corpus_essay_json_link).json() for corpus_essay_json_link in CORPUS_ESSAYS_JSON_LINKS]
+  CORPUS_ESSAYS_JSONS = requests.get(CORPUS_ESSAYS_JSON_LINK).json()
 
   ESSAY_XLSX_COLUMNS = 'essay_id essay_set essay rater1_domain1 rater2_domain1 rater3_domain1 domain1_score rater1_domain2 rater2_domain2 domain2_score rater1_trait1 rater1_trait2 rater1_trait3 rater1_trait4 rater1_trait5 rater1_trait6 rater2_trait1 rater2_trait2 rater2_trait3 rater2_trait4 rater2_trait5 rater2_trait6 rater3_trait1 rater3_trait2 rater3_trait3 rater3_trait4 rater3_trait5 rater3_trait6'.split(' ')
 
@@ -64,19 +53,18 @@ def download_and_convert_uol_corpus_essays():
   essay_id = 0
 
   for essay_set, corpus_essays_json in enumerate(CORPUS_ESSAYS_JSONS):
-    corpus_essays = corpus_essays_json['redacoes']
+    corpus_essays = corpus_essays_json['essays']
 
     for essay in corpus_essays:
       essay_id += 1
 
-      if len(essay['texto']) == 0: continue
+      if len(essay['corrected_text']) == 0: continue
 
       corpus_essays_dict['essay_id'].append(essay_id)
-      corpus_essays_dict['essay_set'].append(1)
-      corpus_essays_dict['essay'].append(essay['texto'])
-      corpus_essays_dict['rater1_domain1'].append(essay['nota'] / 2)
-      corpus_essays_dict['rater2_domain1'].append(essay['nota'] / 2)
-      corpus_essays_dict['domain1_score'].append(essay['nota'])
+      corpus_essays_dict['essay_set'].append(int(essay_id/200))
+      corpus_essays_dict['essay'].append(essay['corrected_text'])
+      corpus_essays_dict['domain1_score'].append(int(essay['points']['knowledge'])/20)
+      corpus_essays_dict['domain2_score'].append(int(essay['points']['cohesion'])/20)
 
   # https://stackoverflow.com/questions/61255750/convert-dictionary-of-dictionaries-using-its-key-as-index-in-pandas
   corpus_essay_dataframe = pd.DataFrame.from_dict(pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in corpus_essays_dict.items() ])))
