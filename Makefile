872356b9e046a42e2fadd147964b049da90d728d
Vanessa <vanessa.ssoares.vs@gmail.com>
2023-07-14 22:28:14 -0300

    Add a docker-compose + makefile + db connection

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ run:
 
 stop:
 	docker compose down
+
+update-requirements:
+	pip3 freeze > backend/src/requirements.txt
--- sql/start-db.sql
+++ sql/start-db.sql
@@ -7,15 +7,17 @@ CREATE TYPE origin AS ENUM (
 
 -- CREATE TABLE
 CREATE TABLE theme (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR NOT NULL
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR NOT NULL
 );
 
 -- CREATE TABLE
 CREATE TABLE text_data (
-    id SERIAL PRIMARY KEY,
-    data VARCHAR NOT NULL,
-    grade SMALLINT,
-    origin origin,
-    theme theme
+    id          SERIAL PRIMARY KEY,
+    data        VARCHAR NOT NULL,
+    analysis    VARCHAR NOT NULL,
+    grade       SMALLINT,
+    origin      origin,
+    theme       theme
+
 );
