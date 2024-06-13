84f0bc39b83817383e985ecf41f4904ff187bd6a
Vanessa <vanessa.ssoares.vs@gmail.com>
2024-06-12 22:22:27 -0300

    add evaluate script

--- fine-tunning/lora/lora.py
+++ fine-tunning/lora/lora.py
@@ -1,4 +1,8 @@
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import numpy as np
+
+#https://www.philschmid.de/fine-tune-flan-t5-peft
 
 # Load dataset from the hub
 dataset = load_dataset("samsum")
@@ -9,14 +13,11 @@ print(f"Test dataset size: {len(dataset['test'])}")
 # Train dataset size: 14732
 # Test dataset size: 819
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-model_id = "google/flan-t5-xxl"
+model_id = "google/flan-t5-xl"
 
 # Load tokenizer of FLAN-t5-XL
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-from datasets import concatenate_datasets
-import numpy as np
+
 
 # The maximum total input sequence length after tokenization.
 # Sequences longer than this will be truncated, sequences shorter will be padded.
@@ -139,36 +140,3 @@ trainer.model.save_pretrained(peft_model_id)
 tokenizer.save_pretrained(peft_model_id)
 # if you want to save the base model to call
 # trainer.model.base_model.save_pretrained(peft_model_id)
-
-import torch
-from peft import PeftModel, PeftConfig
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-# Load peft config for pre-trained checkpoint etc.
-peft_model_id = "results"
-config = PeftConfig.from_pretrained(peft_model_id)
-
-# load base LLM model and tokenizer
-model = AutoModelForSeq2SeqLM.from_pretrained(config.base_model_name_or_path, load_in_8bit=True, device_map={"": 0})
-tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
-
-# Load the Lora model
-model = PeftModel.from_pretrained(model, peft_model_id, device_map={"": 0})
-model.eval()
-
-print("Peft model loaded")
-
-from datasets import load_dataset
-from random import randrange
-
-# Load dataset from the hub and get a sample
-dataset = load_dataset("samsum")
-sample = dataset['test'][randrange(len(dataset["test"]))]
-
-input_ids = tokenizer(sample["dialogue"], return_tensors="pt", truncation=True).input_ids.cuda()
-# with torch.inference_mode():
-outputs = model.generate(input_ids=input_ids, max_new_tokens=10, do_sample=True, top_p=0.9)
-print(f"input sentence: {sample['dialogue']}\n{'---' * 20}")
-
-print(f"summary:\n{tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0]}")
-
