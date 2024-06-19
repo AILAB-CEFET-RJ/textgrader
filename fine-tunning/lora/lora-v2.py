8eb87e44684f367b7f5a38973aa50557680d4701
Vanessa <vanessa.ssoares.vs@gmail.com>
2024-06-19 20:57:53 -0300

    correct target modules

--- fine-tunning/lora/lora-v2.py
+++ fine-tunning/lora/lora-v2.py
@@ -80,7 +80,7 @@ from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, Ta
 lora_config = LoraConfig(
     r=16,
     lora_alpha=32,
-    target_modules=["query_proj", "key_proj","value_proj","pos_dropout","dropout"],
+    target_modules=["query_proj", "key_proj","value_proj"],
     lora_dropout=0.05,
     bias="none",
     task_type=TaskType.SEQ_2_SEQ_LM)
