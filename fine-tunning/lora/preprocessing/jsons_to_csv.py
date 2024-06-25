d3c49df3fd8b2581f39a27537015594db31d5afd
Vanessa <vanessa.ssoares.vs@gmail.com>
2024-06-24 21:02:09 -0300

    update files

--- fine-tunning/lora/preprocessing/jsons_to_csv.py
+++ fine-tunning/lora/preprocessing/jsons_to_csv.py
@@ -15,14 +15,19 @@ def jsons_to_csv(json_dir, csv_file, parquet_file_path=None):
 
     df = pd.DataFrame(columns=["texto","nota", "labels"])
 
+    count = 0
     # Lê cada arquivo JSON e adiciona seus dados à lista
     for file in files:
         file_path = os.path.join(json_dir, file)
+        if count >= 3000:
+            break
         with open(file_path, 'r') as f:
             json_data = json.load(f)
             for obj in json_data:
                 preprocessed = preprocess_texto(obj["texto"])
+                count +=1
                 df = df._append({"texto": preprocessed, "nota": obj["nota"], "labels":int(obj["nota"])}, ignore_index=True)
+
 
     # Salva o DataFrame como um arquivo CSV
     df.to_csv(csv_file, index=False)
@@ -33,8 +38,8 @@ def jsons_to_csv(json_dir, csv_file, parquet_file_path=None):
 # Diretório contendo os arquivos JSON
 json_directory = '../../../textgrader-pt-br/jsons'
 # Nome do arquivo CSV a ser salvo
-csv_file_path = 'output.csv'
-parquet_file_path = 'output-parquet.parquet'
+csv_file_path = 'output-3k.csv'
+parquet_file_path = 'output-parquet-3k.parquet'
 
 # Chama a função para converter JSONs em CSV
 jsons_to_csv(json_directory, csv_file_path, parquet_file_path)
