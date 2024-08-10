ce0b94c0930cef460a7230270cee2f8eb4d23f57
Vanessa <vanessa.ssoares.vs@gmail.com>
2024-08-10 18:23:40 -0300

    print batch

--- fine-tunning/lora-peft-confusion-matrix-competencies-v2.py
+++ fine-tunning/lora-peft-confusion-matrix-competencies-v2.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     for epoch in range(config.num_epochs):
         model.train()
         for step, batch in enumerate(train_dataloader):
-            print(batch)
+            print(batch["labels"])
             batch.to(config.device)
             outputs = model(**batch)
             loss = outputs.loss
@@ -137,8 +137,10 @@ if __name__ == '__main__':
             optimizer.zero_grad()
 
         model.eval()
+
+        print("-"*100)
         for step, batch in enumerate(tqdm(test_dataloader)):
-            print(batch)
+            print(batch["labels"])
             batch.to(config.device)
             with torch.no_grad():
                 outputs = model(**batch)
