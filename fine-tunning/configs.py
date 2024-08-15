2d5df877e2f210d934fe804ed778787e95747526
Vanessa <vanessa.ssoares.vs@gmail.com>
2024-08-14 21:29:13 -0300

    model

--- fine-tunning/configs.py
+++ fine-tunning/configs.py
@@ -14,7 +14,7 @@ class Configs:
         print("Definindo configs...")
         self.obs = None
         self.batch_size = 4
-        self.model_name_or_path = "meta-llama/Llama-Guard-3-8B-INT8" #"FacebookAI/xlm-roberta-base"
+        self.model_name_or_path = "xlm-roberta-base"
         # "neuralmind/bert-large-portuguese-cased"
         # "FacebookAI/xlm-roberta-base"
         # "google-bert/bert-base-multilingual-cased"
